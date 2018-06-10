854d3b29c8e05bc11363fe3966df6b1db1c24e4f
William Vukasovic <wjbodyv@gmail.com>
2018-06-10 10:24:01 -0500

    major changes to design. going with a parallax design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'about', component: AboutMeComponent},
-  {path: 'projects', component: ProjectsComponent},
-  {path: 'home', component: HomeComponent}
 ]
 
 @NgModule({
