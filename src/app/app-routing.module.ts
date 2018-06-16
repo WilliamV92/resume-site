d8e89a20d20c747f0f2042f02b313bf32987287e
William Vukasovic <wjbodyv@gmail.com>
2018-06-16 15:48:57 -0500

    animation for router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutMeComponent },
-  { path: 'projects', component: ProjectsComponent },
+  { path: 'home', component: HomeComponent, data: { page: 'one' } },
+  { path: 'about', component: AboutMeComponent, data: { page: 'two' } },
+  { path: 'projects', component: ProjectsComponent, data: { page: 'three' } },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ]
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,43 @@
-import { Component, AfterViewInit, HostListener, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
+import { trigger, style, animate, transition, group, query, animateChild } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
+  animations: [
+    trigger('animRoutes', [
+      transition('* <=> *', [
+        group([
+          query(
+            ':enter',
+            [
+              style({
+                transform: 'translateX(-100%)'
+              }),
+              animate(
+                '200ms ease-in',
+                style({transform: 'translateX(0%)' })
+              ),
+              animateChild()
+            ],
+            { optional: true }
+          ),
+          query(
+            ':leave',
+            [animate('200ms ease-in', style({transform: 'translateX(100%)' })), animateChild()],
+            { optional: true }
+          )
+        ])
+      ])
+    ])
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
-  @ViewChild("fixedContainer") fixedContainer;
-  @ViewChild("fixedHeader") fixedHeader;
-  fadeIn: boolean = false;
-  headerStick: boolean = false;
-  cachedOffset: number = 0;
-  
-  ngAfterViewInit(): void {
+export class AppComponent {
+
+  getPage(outlet) {
+    console.log(outlet);
+    console.log(outlet.activatedRouteData['page'] || 'one');
+    return outlet.activatedRouteData['page'] || 'one';
   }
 }
